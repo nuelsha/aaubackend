cef798e50ff6e71aad61202b6341daa327078b0e
nuelsha <mezemirnuel@gmail.com>
2025-07-22 22:19:28 +0300

    fix: Update CORS origin to production URL

--- src/app.js
+++ src/app.js
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "'https://aaufrontend-v2.vercel.app'",
     credentials: true,
   })
 );
